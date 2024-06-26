ec891f8c210eecac47e3d6da0d45abc609c50e74
Joshua Moore <47381009+an0moly@users.noreply.github.com>
2024-06-25 21:16:24 -0700

    Update Constants.cs

--- Windows/MultilanguageChat/Common/Constants.cs
+++ Windows/MultilanguageChat/Common/Constants.cs
@@ -1,11 +1,11 @@
-﻿namespace MultilanguageChat.Common
+namespace MultilanguageChat.Common
 {
-    public class Constants
+    public static class Constants
     {
-        public const string ServerUrl = "";
+        public const string ServerUrl = "defcontranslator.azurewebsites.net\\chat";
 
-        public const string TranslatorSubscriptionKey = "";
-        public const string SpeechRegion = "";
-        public const string SpeechSubscriptionKey = "";
+        public const string TranslatorSubscriptionKey = "6257fd1312984423bef7040d7c4e990c";
+        public const string SpeechRegion = "westus2";
+        public const string SpeechSubscriptionKey = "90c36f5357994870970398a6c6e593a2";
     }
 }
